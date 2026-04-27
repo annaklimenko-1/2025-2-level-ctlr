2939b66637b1f2efb0b1d8ba423697145da3e9c8
annaklimenko-1 <afklimenko@edu.hse.ru>
2026-04-27 10:13:55 +0300

    seminars

--- seminars/seminar_04_06_2026/try_requests.py
+++ seminars/seminar_04_06_2026/try_requests.py
@@ -12,21 +12,19 @@ except ImportError:
 
 if __name__ == "__main__":
     # Step 1. GET request
-    url = "https://www.nn.ru/"
+    url = "https://lit.lib.ru/"
     response = requests.get(url)
     print(f"Status code: {response.status_code}")
     print(f"First 500 chars:\n{response.text[:500]}...")
 
-    # Step 2. Response encoding
-    response.encoding = "utf-8"
-    print(f"First 500 chars with utf-8:\n{response.text[:500]}...")
+
 
     # Step 3. Adding headers (simulate browser request, some sites may respond better)
     # View my headers at: https://www.whatismybrowser.com/detect/what-http-headers-is-my-browser-sending/
     headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 YaBrowser/26.3.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept-Language": "ru,en;q=0.9",
     }
     response = requests.get(url, headers=headers)
     print(f"With headers status: {response.status_code}")
@@ -42,7 +40,6 @@ if __name__ == "__main__":
 
     # Step 5. Response handling
     if response.ok:
-        response.encoding = "utf-8"
         with open("page.html", "w", encoding="utf-8") as f:
             f.write(response.text)
         print("Page saved successfully")
