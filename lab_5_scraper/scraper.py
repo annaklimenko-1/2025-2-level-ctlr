8e422417b8f1cf93db36290ce08bb63525193109
annaklimenko-1 <afklimenko@edu.hse.ru>
2026-05-17 10:52:27 +0300

    data

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -313,18 +313,24 @@ class Crawler:
             except requests.RequestException:
                 continue
 
-            if response.status_code != 200:
+            if not response.ok:
                 continue
 
             soup = BeautifulSoup(response.text, 'html.parser')
-            url = self._extract_url(soup)
-
-            if url and url not in self.urls:
-                if url.startswith('/'):
-                    full_url = self.base_url + url
-                    self.urls.append(full_url)
-                elif url.startswith('http'):
-                    self.urls.append(url)
+        
+            for link in soup.find_all('a', href=True):
+                if len(self.urls) >= required_count:
+                    break
+                
+                href = link.get('href', '')
+                if 'text_' in href:
+                    if href.startswith('/'):
+                        full_url = self.base_url + href
+                        if full_url not in self.urls:
+                            self.urls.append(full_url)
+                    elif href.startswith('http'):
+                        if href not in self.urls:
+                            self.urls.append(href)
     def get_search_urls(self) -> list:
         """
         Get seed_urls param.
