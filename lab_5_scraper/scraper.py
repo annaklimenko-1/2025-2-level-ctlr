bd67c065c78492eb581c3091a584616ff9cca6bc
annaklimenko-1 <afklimenko@edu.hse.ru>
2026-05-22 19:23:01 +0300

    crarlerrrrr

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -273,7 +273,6 @@ class Crawler:
         self.config = config
         self.urls: list[str] = []
         self.base_url: str = ""
-        self.url_pattern = re.compile(r'/[a-z]/[a-z_]+/text_\d+\.shtml')
 
     def _extract_url(self, article_bs: Tag) -> str:
         """
@@ -285,11 +284,6 @@ class Crawler:
         Returns:
             str: Url from HTML
         """
-        for link in article_bs.find_all('a', href=True):
-            href = link.get('href', '')
-            if '.shtml' in href or 'text_' in href:
-                if href.startswith('/') or href.startswith('http'):
-                    return href
         return ""
 
     def find_articles(self) -> None:
@@ -302,7 +296,7 @@ class Crawler:
         for seed_url in seed_urls:
             if len(self.urls) >= required_count:
                 break
-
+L
             match = re.match(r'(https?://[^/]+)', seed_url)
             if match:
                 self.base_url = match.group(1)
@@ -318,23 +312,27 @@ class Crawler:
                 continue
 
             soup = BeautifulSoup(response.text, 'html.parser')
-            
+ 
             for link in soup.find_all('a', href=True):
                 if len(self.urls) >= required_count:
                     break
                     
                 href = link.get('href', '')
-                
+
                 if href.startswith('/'):
                     full_url = self.base_url + href
                 elif href.startswith('http'):
                     full_url = href
                 else:
                     continue
-
-                if self.url_pattern.search(full_url):
+ 
+                if (full_url.startswith(self.base_url) and 
+                    'indexdate.shtml' not in full_url and 
+                    full_url.endswith('.shtml')):
+                    
                     if full_url not in self.urls:
                         self.urls.append(full_url)
+
     def get_search_urls(self) -> list:
         """
         Get seed_urls param.
