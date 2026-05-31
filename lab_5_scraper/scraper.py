d308a382a0d4041c469fedf53c873a2a4be899b6
annaklimenko-1 <afklimenko@edu.hse.ru>
2026-06-01 01:10:42 +0300

    site please be alive plsss

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -6,8 +6,10 @@ Crawler implementation.
 
 import datetime
 import json
+import random
 import re
 import shutil
+import time
 from pathlib import Path
 
 import requests
@@ -252,6 +254,7 @@ def make_request(url: str, config: Config) -> requests.models.Response:
         verify=config.get_verify_certificate(),
     )
     response.encoding = config.get_encoding()
+    time.sleep(random.uniform(2, 5))
     return response
 
 
